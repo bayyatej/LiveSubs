eed56cad8b75e47478082423dcc182c21275db2d
tongkevn <tongkevn@umich.edu>
2019-01-19 03:44:05 -0500

    Setup Node.js and Google Cloud SDK. Added some WebRTC stuff.

---
+++ public/scripts/client.js
@@ -0,0 +1,156 @@
+
+// Chat platform
+const chatTemplate = Handlebars.compile($('#chat-template').html());
+const chatContentTemplate = Handlebars.compile($('#chat-content-template').html());
+const chatEl = $('#chat');
+const formEl = $('.form');
+const messages = [];
+let userName = 'Undefined User';
+
+// Local Video
+const localImageEl = $('#localImage');
+const localVideoEl = $('#localVideo');
+
+// Remote Videos
+const remoteVideoTemplate = Handlebars.compile($('#remote-video-template').html());
+const remoteVideosEl = $('#remoteVideos');
+let remoteVideosCount = 0;
+
+// Register new Chat Room
+const createRoom = (roomID) => {
+    webrtc.createRoom(roomID, (err, name) => {
+        showChatRoom(name);
+    });
+};
+
+// Join existing Chat Room
+const joinRoom = (roomID) => {
+    webrtc.joinRoom(roomID);
+    showChatRoom(roomID);
+};
+
+// Post Local Message
+const postClientMessage = (message) => {
+    const msg = {
+        userName,
+        message
+    };
+
+    // Send message to all peers.
+    webrtc.sendToAll('chat', msg);
+    // Clear chat input field.
+    $('#msgField').val('');
+    // Update our message list locally.
+    messages.push(msg);
+    updateChatMessages();
+};
+
+// Display Chat Interface
+const showChatRoom = (room) => {
+    // Hide room join form.
+    formEl.hide();
+    const html = chatTemplate({ room });
+    chatEl.html(html);
+    const postForm = $('form');
+
+    // Post Message Validation Rules
+    postForm.form({
+        message: 'empty',
+    });
+
+    // Post message for joining user.
+    const joinMsg = {
+        userName: 'Room',
+        message: 'Hello, ' + userName + '. Welcome to ' + room + '!'
+    };
+
+    messages.push(joinMsg);
+    updateChatMessages();
+
+    $('#submitMsgBtn').on('click', () => {
+        const message = $('#msgField').val();
+        postClientMessage(message);
+    });
+    $('#msgField').on('keydown', (event) => {
+        // Add listener for enter key.
+        if (event.keyCode === 13) {
+            const message = $('#msgField').val();
+            postClientMessage(message);
+        }
+    });
+};
+
+// Update Chat Messages
+const updateChatMessages = () => {
+    const html = chatContentTemplate({ messages });
+    const chatContentEl = $('#chatList');
+    chatContentEl.html(html);
+
+    // automatically scroll to the bottom.
+    const scrollHeight = chatContentEl.prop('scrollHeight');
+    chatContentEl.animate({ scrollTop: scrollHeight }, 150);
+};
+
+// create our WebRTC connection
+const webrtc = new SimpleWebRTC({
+    // the id/element dom element that will hold "our" video
+    localVideoEl: 'localVideo',
+    // the id/element dom element that will hold remote videos
+    remoteVideosEl: 'remoteVideos',
+    // immediately ask for camera access
+    autoRequestMedia: true,
+});
+
+window.addEventListener('load', () => {
+    // Add validation rules to Create/Join Room Form
+    formEl.form({
+        fields: {
+            roomID: 'empty',
+            userName: 'empty',
+        },
+    });
+
+    // We got access to local camera
+    webrtc.on('localStream', () => {
+        localImageEl.hide();
+        localVideoEl.show();
+    });
+
+    $('.submit').on('click', (event) => {
+        if (!formEl.form('is valid')) {
+            return false;
+        }
+        userName = $('#userName').val();
+        const roomID = $('#roomID').val();
+        if (event.target.id === 'createBtn') {
+            createRoom(roomID);
+        }
+        else {
+            joinRoom(roomID);
+        }
+        return false;
+    });
+
+    // Receive message from remote user
+    webrtc.connection.on('message', (data) => {
+        if (data.type === 'chat') {
+            const message = data.payload;
+            messages.push(message);
+            updateChatMessages();
+        }
+    });
+
+    // Remote video was added
+    webrtc.on('videoAdded', (video, peer) => {
+        const id = webrtc.getDomId(peer);
+        const html = remoteVideoTemplate({ id });
+        if (remoteVideosCount === 0) {
+            remoteVideosEl.html(html);
+        } else {
+            remoteVideosEl.append(html);
+        }
+        $(`#${id}`).html(video);
+        $(`#${id} video`).addClass('ui image medium');
+        remoteVideosCount += 1;
+    });
+});
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 8080;
+
+app.all('/', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+// Set 'public' folder as root
+app.use(express.static('public'));
+// Provide access to node_modules folder from the client-side
+app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+// Redirect all traffic to index.html
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.listen(port, () => {
+    console.log('listening on ' + port);
+});
